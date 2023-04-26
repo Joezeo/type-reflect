25e97996af6212b91ebb16fe788bb6017ed2132b
Joe Zane <joezane.cn@gmail.com>
2023-04-26 10:31:05 +0800

    1. code update

--- tests/bevy_reflect_test_beta.rs
+++ tests/bevy_reflect_test_beta.rs
@@ -1,4 +1,6 @@
-use bevy_reflect::{reflect_trait, Reflect, TypeRegistry};
+#![feature(trivial_bounds)]
+use std::{any::Any, ptr::NonNull};
+use bevy_reflect::{reflect_trait, Reflect, TypeRegistry, FromReflect};
 
 #[reflect_trait]
 trait DoA {
@@ -8,6 +10,23 @@ trait DoA {
 #[reflect_trait]
 trait DoB {
     fn do_b(&self);
+}
+
+#[reflect_trait]
+trait DoC {
+    fn do_c(&self);
+}
+
+#[derive(Reflect, FromReflect)]
+struct Bazz {
+    do_c: Box<dyn DoC>,
+    p: Option<NonNull<dyn DoC>>,
+    non: NonRe,
+}
+
+#[derive(Reflect)]
+struct NonRe {
+
 }
 
 #[derive(Reflect)]
@@ -38,6 +57,7 @@ fn main() {
     type_registry.register_type_data::<Foo, ReflectDoB>();
     type_registry.register::<Bar>();
     type_registry.register_type_data::<Bar, ReflectDoB>();
+    type_registry.register::<NonRe>();
 
     let list: Vec<Box<dyn Reflect>> = vec![Box::new(Foo), Box::new(Bar)];
 
@@ -49,5 +69,11 @@ fn main() {
         if let Some(reflect) = type_registry.get_type_data::<ReflectDoB>(item_ref.type_id()) {
             (reflect.get_func)(item_ref).unwrap().do_b()
         }
+    }
+}
+
+fn _type_cast<T: DoA + Any + Reflect>(type_registry: &TypeRegistry, obj: &T) {
+    if let Some(reflect) = type_registry.get_type_data::<ReflectDoB>(obj.type_id()) {
+        (reflect.get_func)(obj).unwrap().do_b()
     }
 }
